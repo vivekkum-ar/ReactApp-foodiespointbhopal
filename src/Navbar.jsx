4e78a7f0329612368908c0521cb8ced59b9397d5
Vivek Kumar <vivekkr9119@gmail.com>
2023-02-25 19:21:10 +0530

    Nav with Bootstrap & FAS

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,48 +4,65 @@ import "./index.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBlog, faBurger, faCheckSquare, faCoffee, faHome, faPhone, faUtensils } from '@fortawesome/free-solid-svg-icons'
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 library.add(faHome,faBlog,faCheckSquare,faUtensils,faPhone)
 
 function Navbar() {
   return (
-    <nav className="relative w-full flex flex-wrap items-center justify-between py-4 bg-gray-100 text-gray-500 hover:text-gray-700 focus:text-gray-700 shadow-lg navbar navbar-expand-lg navbar-light">
-      <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-        <button className=" navbar-toggler text-gray-500 border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" className="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path fill="currentColor" d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"></path>
-          </svg>
+    <nav className="navbar navbar-expand-lg navbar-light shadow sticky-top bg-light  py-0 my-0">
+      <div className="container-fluid">
+        <a className="font-poppins fw-bold text-prime-gradient navbar-brand" href="#">
+          <img
+            src="/images/logo-navbar.png"
+            alt=""
+            width="50"
+            height="24"
+            className="d-inline-block align-text-center me-1"
+          />
+          FoodiesPoint
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        
-        <div className="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
-
-          <Link className="text-xl text-black" to="/">
-          <img src="/images/logo.jpg" className="pr-2 rounded-pill" width={60}/>
-            {/* Foodies Point */}
-          </Link>
-          <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
-          <li className="nav-item pr-2">
-              <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/">
-            <FontAwesomeIcon icon={faHome} className=""/>
-            {/* Alternatively, <FontAwesomeIcon icon="fa-solid fa-home"> */}
+        <div className="collapse navbar-collapse align-items-center" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item text-prime">
+              <Link className="nav-link font-poppins text-prime active" to="/">
+                <FontAwesomeIcon icon={faHome} className=" pe-1" />
+                {/* Alternatively, <FontAwesomeIcon icon="fa-solid fa-home"> */}
                 Home
               </Link>
             </li>
-            <li className="nav-item pr-2">
-              <Link className="nav-link disabled text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/about">
-            <FontAwesomeIcon icon="fa-solid fa-utensils" className=""/>
+            <li className="nav-item disabled">
+            <Link className="nav-link font-poppins text-prime" to="/about">
+                <FontAwesomeIcon icon={faUtensils} className="pe-1" />
+                {/* Alternatively, <FontAwesomeIcon icon="fa-solid fa-home"> */}
                 About
               </Link>
             </li>
-            <li className="nav-item pr-2">
-              <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/contact">
-              <FontAwesomeIcon icon="fa-solid fa-phone" className=""/>
+            <li className="nav-item disabled">
+            <Link className="nav-link font-poppins text-prime" to="/contact">
+                <FontAwesomeIcon icon={faPhone} className="pe-1" />
+                {/* Alternatively, <FontAwesomeIcon icon="fa-solid fa-home"> */}
                 Contact
               </Link>
             </li>
-            <li className="nav-item pr-2 ">
-              <Link className="nav-link disabled text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/blogs">
-              <FontAwesomeIcon icon="fa-solid fa-blog" className=""/>
+            <li className="nav-item disabled">
+            <Link className="nav-link font-poppins text-prime" to="/blogs">
+                <FontAwesomeIcon icon={faBlog} className="pe-1" />
+                {/* Alternatively, <FontAwesomeIcon icon="fa-solid fa-home"> */}
                 Blogs
               </Link>
             </li>
